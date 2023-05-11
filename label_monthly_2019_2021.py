f93d8d0e5151941f527de2bf4ec7ad72890225b0
Yizheng Chen <surrealyz@gmail.com>
2023-05-11 15:06:36 -0700

    process families from 2019-2021

--- label_monthly_2019_2021.py
+++ label_monthly_2019_2021.py
@@ -8,7 +8,7 @@ from ratelimit import limits, sleep_and_retry
 from subprocess import Popen, PIPE
 
 def main():
-    for year in range(2019, 2020):
+    for year in range(2019, 2022):
         for idx, month in enumerate(['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']):
             apk_dir = '/data2/yizheng/android/malware/%s/%s' % (year, month)
             output_file = '/home/yz/code/mal/src/dataset/avclass/android/%s_%s.txt' % (year, month)
