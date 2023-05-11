78f0081bda4b368146c969fff9c7c73394742f84
Yizheng Chen <surrealyz@gmail.com>
2023-05-11 14:59:31 -0700

    update labeling scripts with tag of the label

--- label_monthly_2019_2021.py
+++ label_monthly_2019_2021.py
@@ -8,12 +8,12 @@ from ratelimit import limits, sleep_and_retry
 from subprocess import Popen, PIPE
 
 def main():
-    for year in range(2012, 2019):
+    for year in range(2019, 2020):
         for idx, month in enumerate(['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']):
             apk_dir = '/data2/yizheng/android/malware/%s/%s' % (year, month)
             output_file = '/home/yz/code/mal/src/dataset/avclass/android/%s_%s.txt' % (year, month)
             # ./avclass2/avclass2_labeler.py -staticdir /space1/android/malware/2012/01/ -vt3 > /home/yz/code/mal/src/dataset/avclass/android/2012_01.txt
-            cmd = './avclass2/avclass2_labeler.py -staticdir %s -p -vt3 > %s' % (apk_dir, output_file)
+            cmd = './avclass2/avclass2_labeler.py -staticdir %s -p -vt3 -hash sha256 > %s' % (apk_dir, output_file)
             print(cmd)
             os.system(cmd)
 
